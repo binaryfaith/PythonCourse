2c7a119f2818326563ac066295fc1d3bc22b02c9
binaryfaith <binaryfaith@gmail.com>
2017-10-02 18:13:58 -0700

    finishmath

---
+++ multiples.py
@@ -0,0 +1,14 @@
+#Multiples#
+#Part1#
+for count in range(1,1000,2):
+    print "looping - ", count
+
+for count in range(5,1000000,5):
+    print "looping - ", count
+
+#Sum List#
+a = [1,2,5,10,255,3]
+print sum(a)
+#Average List#
+a =[1,2,5,10,255,3]
+print (sum(a))/(len(a))
