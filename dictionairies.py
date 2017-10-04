1ee091566c84df85a637fa5af14616f086fb535f
binaryfaith <binaryfaith@gmail.com>
2017-10-03 20:42:00 -0700

    dictup

---
+++ dictionairies.py
@@ -0,0 +1,8 @@
+def my_details ():
+    stuff={"name":"David","age":"35","country":"USA","language":"english"}
+    print stuff
+    print "My name is " + stuff["name"]
+    print "My age is " + stuff["age"]
+    print "My country of birth is the " + stuff["country"]
+    print "My favorite language is " + stuff["language"]
+my_details()
